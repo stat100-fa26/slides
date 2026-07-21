4937c2fe68cf52d907d3c7b4102c035b76f92dab
mdogucu <mdogucu@gmail.com>
2026-07-21 16:09:39 -0400

    try fixing the slide menu button accessibility

--- filters/fix-empty-links.lua
+++
@@ -1,9 +0,0 @@
--- fix-empty-links.lua
-function Link(el)
-  -- el.target is the href value for HTML output
-  if el.target == "" then
-    -- Remove the link but keep the content (if any)
-    return el.content
-  end
-  return el
-end
